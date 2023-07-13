5f5f575ee69ffd1d86f659d446ea9d50970f3dfc
Alexis Martin <alexismartin1895@gmail.com>
2023-07-13 15:03:27 -0700

    fixed p8 file

--- routes/apple.js
+++ routes/apple.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
-const authToken = fs.readFileSync('AuthKey_6MU3D8962Y.p8').toString();
-const team_id = 'BLSXY36MFR';
-const key_id = '6MU3D8962Y';
+// const authToken = fs.readFileSync("AuthKey_6MU3D8962Y.p8").toString();
+// console.log(typeof authToken, authToken);
+const authToken = process.env.auth_key;
+const team_id = "BLSXY36MFR";
+const key_id = "6MU3D8962Y";
 
 const token = jwt.sign({}, authToken, {
-	algorithm: 'ES256',
-	expiresIn: '180d',
+	algorithm: "ES256",
+	expiresIn: "180d",
 	issuer: team_id,
 	header: {
-		alg: 'ES256',
+		alg: "ES256",
 		kid: key_id,
 	},
 });
 
-router.get('/token', function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
+router.get("/token", function (req, res) {
+	res.setHeader("Content-Type", "application/json");
 	res.send(JSON.stringify({ token: token }));
 });
 
